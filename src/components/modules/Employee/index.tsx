7263f4cf1887ae05643375f5d30a6423b30e5a4f
Divan <divanvant@gmail.com>
2023-01-06 16:29:38 +0200

    chore: assignment 3

--- src/components/modules/Employee/index.tsx
+++ src/components/modules/Employee/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import Image from 'next/image';
 
-import { EmployeeContainer } from './styled';
+import {
+  EmployeeContainer,
+  EmployeeHeading,
+  EmployeeInfoContainer,
+  EmployeeSubHeading,
+} from './styled';
 import { Employee as EmployeeType } from 'queries/types';
 
 type Props = {
@@ -11,11 +16,18 @@ type Props = {
 export const Employee: React.FC<Props> = ({ employee }) => {
   return (
     <EmployeeContainer>
+      <EmployeeInfoContainer>
+        <EmployeeHeading>{employee.value.name}</EmployeeHeading>
+        <EmployeeSubHeading>{employee.value.function}</EmployeeSubHeading>
+      </EmployeeInfoContainer>
+
       <Image
         src={`https://cms.labela.nl${employee.value.image.small}`}
         width={488}
         height={800}
         alt={employee.value.image.alt}
+        objectFit="cover"
+        objectPosition="top"
       />
     </EmployeeContainer>
   );
--- src/components/modules/Employee/styled.ts
+++ src/components/modules/Employee/styled.ts
@@ -5,4 +5,25 @@ export const EmployeeContainer = styled.div`
   overflow: hidden;
   display: flex;
   align-items: center;
+  flex-flow: column;
+  position: relative;
+  z-index: 0;
+`;
+
+export const EmployeeInfoContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 1;
+  padding: 0.75rem 1rem;
+`;
+
+export const EmployeeHeading = styled.h2`
+  margin: 0 auto 0.5rem;
+`;
+
+export const EmployeeSubHeading = styled.h3`
+  margin-top: 0;
 `;
